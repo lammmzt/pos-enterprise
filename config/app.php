fadf316c6bc3593cef3035a5f265138b59b8bce8
lammmzt <alam0098@stmik-wp.ac.id>
2026-02-17 15:07:15 +0700

    add fiture

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://seblak-bucin.sman1pekalongan.sch.id/'),
 
     /*
     |--------------------------------------------------------------------------
